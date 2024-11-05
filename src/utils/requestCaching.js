005c8c29fc851a00fe860779a417bc82035ba74b
Lê Gia Phú <phugiale2912@gmail.com>
2024-11-05 16:56:55 +0700

    feat: add caching only for 200 status

--- src/utils/requestCaching.js
+++ src/utils/requestCaching.js
@@ -4,13 +4,20 @@
 const apicache = require("apicache");
 
 // --------------------------
-// Section: in
+// Section: Reponse Caching Modes (In-Memory Version)
 // --------------------------
+/* Define `cache` from `apicache` middleware */
 const cache = apicache.middleware;
+/* Options for `cache` function */
+const onlyStatus200 = (req, res) => {
+  return res.statusCode === 200;
+};
+/* In-Memory Reponse Caching Modes */
 const cache5Minutes = cache("5 minutes");
 const cache15Minutes = cache("15 minutes");
+const cache5Minutes200Status = cache("5 minutes", onlyStatus200);
 
 // --------------------------
 // Section: Exports
 // --------------------------
-module.exports = { cache5Minutes, cache15Minutes };
+module.exports = { cache5Minutes, cache15Minutes, cache5Minutes200Status };
